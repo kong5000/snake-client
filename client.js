fe293ed982fecd10a04bd002b1e72d08bec2dc14
Keith Ong <keith.ong5000@gmail.com>
2020-08-26 19:46:10 +0000

    Add succesful connection callback

---
+++ client.js
@@ -0,0 +1,25 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function () {
+  const conn = net.createConnection({
+    host: '135.23.222.131',
+    port: 50542
+  });
+  conn.on('connect', () => {
+    console.log("Successful connection!");
+    conn.write('Name: US');
+  })
+
+  conn.on('data', (data) => {
+    console.log(data)
+  })
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
+
+  return conn;
+}
+
+module.exports = {connect};
