b54ee55a172fe5627903053ed0c34f55ffad978d
Keith Ong <keith.ong5000@gmail.com>
2020-08-26 20:37:36 +0000

    Refactor play.js

--- play.js
+++ play.js
@@ -5,23 +5,27 @@
 const readline = require('readline');
 readline.emitKeypressEvents(process.stdin);
 
+const handleUserInput = (str, key) => {
+  if (key.ctrl && key.name === 'c') {
+    process.exit();
+  }
+}
 const setupInput = function() {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
-  stdin.on('keypress', (str, key) => {
-    if (key.ctrl && key.name === 'c') {
-      process.exit();
-    }
-  });
+  stdin.on('keypress', handleUserInput);
+  // stdin.on('keypress', (str, key) => {
+  //   if (key.ctrl && key.name === 'c') {
+  //     process.exit();
+  //   }
+  // });
   return stdin;
 }
 setupInput();
 
-const handleUserInput = (chunk,key) => {
-  console.log("helllo hello")
-}
+
 
 const { connect } = require('./client');
 console.log('Connecting ...');
